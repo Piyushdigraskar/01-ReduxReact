b8b7c154325be766c043edcff206ef93ef927c61
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-03-18 13:44:24 +0530

    first commit

---
+++ src/Store/index.js
@@ -0,0 +1,19 @@
+import { createStore } from "redux";
+
+const counterReducer = (state = {counter: 0}, action)=>{
+    if(action.type === 'increment'){
+        return {
+            counter: state.counter + 5
+        }
+    }
+    if(action.type === 'decrement'){
+        return {
+            counter: state.counter - 5
+        }
+    }
+    return state;
+}
+
+const Store = createStore(counterReducer);
+
+export default Store;
